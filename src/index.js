a58aedf29bb05545434ca49815580045b9adbc5e
Ngoc Phan <nphan4@yahoo.com>
2019-01-12 19:49:11 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.log(error)
+  );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
+  return (
+    <div>
+      Hi There!
+      <SeasonDisplay />
+    </div>
+  )
+};
+
+ReactDOM.render(
+  <App />, 
+  document.querySelector('#root')
+);

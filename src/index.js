805a24bbcb38d3a7a2e1592f92b906dd35a26b64
Ngoc Phan <nphan4@yahoo.com>
2019-01-16 01:17:11 -0700

    Refractored fetch into component-did-mount

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+  state = {
       latitude: null,
       errorMessage: ''
-    };
+    }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({latitude: position.coords.latitude})
-      },
-      (error) => {
-        this.setState({errorMessage: error.message})
-      }
+      position => this.setState({ latitude: position.coords.latitude }),
+      error => this.setState({ errorMessage: error.message })
     )
+  }
+
+  componentDidUpdate() {
+    console.log('just updated')
   }
 
   render() {

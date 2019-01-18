e07d850bcf59416252502008d748c69231f919c6
Ngoc Phan <nphan4@yahoo.com>
2019-01-17 17:49:31 -0700

    Function that determines season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
+const getSeason = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? 'summer': 'winter';
+  } else {
+    return latitude > 0 ? 'winter': 'summer';
+  }
+}
+
 const SeasonDisplay = (props) => {
+  const season = getSeason(props.latitude, new Date().getMonth())
   return (
     <div>Latitude: {props.latitude}</div>
   )
